6c3fe3d87e087d7c7f82151f663c4cd9fc32b24a
Kent Sommer <kent.sommer13@gmail.com>
2018-01-03 09:46:19 +0900

    Patch 2 for sublime

--- install_core.sh
+++ install_core.sh
@@ -665,7 +665,7 @@ fi
 ##################
 installed=$(dpkg -s sublime-text | grep "ok installed")
 if [ "" == "$installed" ]; then
-  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -y -
+  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -y --allow-unauthenticated -
   sudo apt install -y apt-transport-https
   echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
   sudo apt update
